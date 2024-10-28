a052e69bbb7e81609170a6a66028112aec0761bb
seulGit <seulgi9421@gmail.com>
2024-10-28 16:55:15 +0900

    241028 - 문자열 붙여서 출력하기

--- src/main/java/org/example/programmers/Test01.java
+++ src/main/java/org/example/programmers/Test01.java
@@ -1,7 +1,24 @@
 package org.example.programmers;
 
 public class Test01 {
+
+    /**
+     *
+     * 문제 : 주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다.
+     * 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해 주세요.
+     *
+     *
+     * 출력 예시 :
+     * 3
+     * 2
+     * 1
+     * Let's go!
+     *
+     */
     public Test01(int launcher) {
-        System.out.println("This is site 1");
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
     }
 }
