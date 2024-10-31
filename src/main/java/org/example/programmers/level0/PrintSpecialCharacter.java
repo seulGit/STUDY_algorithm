5ba024404cd58890a21ce9b8e3d7ca12b1f30ef3
seulGit <seulgi9421@gmail.com>
2024-10-31 16:06:08 +0900

    241031 - 특수문자 출력 문제

---
+++ src/main/java/org/example/programmers/level0/PrintSpecialCharacter.java
@@ -0,0 +1,21 @@
+package org.example.programmers.level0;
+
+/**
+ *
+ * 문제 : 다음과 같이 출력하도록 코드를 작성해 주세요.
+ *
+ * 출력 예시 :
+ * !@#$%^&*(\'"<>?:;
+ *
+ */
+public class PrintSpecialCharacter {
+
+    public static void main(String[] args) {
+        PrintSpecialCharacter printSpecialCharacter = new PrintSpecialCharacter();
+        printSpecialCharacter.solution();
+    }
+
+    private void solution() {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
